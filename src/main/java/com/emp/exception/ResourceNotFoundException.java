bff0f4f3cb736c934bdbd15b7eebd9daeab0d7da
cogjava3109 <cogjava3109@ctsjava1131.iiht.tech>
2022-08-17 10:52:32 +0530

    add resourceNotFoundException

---
+++ src/main/java/com/emp/exception/ResourceNotFoundException.java
@@ -0,0 +1,28 @@
+package com.emp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	private String resourceName;
+	private String fieldName;
+	private Object fieldValue;
+	
+	public String getResourceName() {
+		return resourceName;
+	}
+	
+	public String getFieldName() {
+		return fieldName;
+	}
+	
+	public Object getFieldValue() {
+		return fieldValue;
+	}
+	
+	
+	
+}
